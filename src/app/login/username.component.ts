442cf3095bf8d3b5d5991bf431f362f4e5909ab0
Rory Claasen <t-roclaa@microsoft.com>
2017-11-17 14:50:46 +0000

    Updated code (still learning angular

--- src/app/login/username.component.ts
+++ src/app/login/username.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
 })
 export class UsernameComponent {
     username: string = '';
+    inputLength: number = 24;
+
+    validate(): void {
+        var username = this.username;
+        var length = username.length;
+        if (length > 0) {
+
+        }
+    }
 }
