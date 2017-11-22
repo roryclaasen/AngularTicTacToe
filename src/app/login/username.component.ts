851d98f6c1a3085872e921abe5a360ccbf1018d1
Rory Claasen <t-roclaa@microsoft.com>
2017-11-22 15:13:08 +0000

    update login page and change buil.prod command

--- src/app/login/username.component.ts
+++ src/app/login/username.component.ts
@@ -4,15 +4,42 @@ import { Component } from '@angular/core';
     selector: 'app-content',
     templateUrl: './username.component.html'
 })
+
 export class UsernameComponent {
-    username: string = '';
+    header: string = 'Tic-Tac-Toe';
+
+    inputPrompt: string = 'Enter your username';
+    inputLabel: string = 'Username';
+    input: string = '';
     inputLength: number = 24;
+    inputType: string = 'text';
+
+    username: string = '';
+    gamePin: string = '';
 
     validate(): void {
-        var username = this.username;
-        var length = username.length;
+        var input = this.input;
+        var length = input.length;
         if (length > 0) {
-
+            if (this.username.length == 0) {
+                this.username = this.header = input;
+                this.input = '';
+                this.inputPrompt = 'Enter the Game Pin or Create a server';
+                this.inputLabel = 'Game Pin';
+                this.inputLength = 6;
+                this.inputType = 'number';
+            } else {
+                this.join(input);
+            }
         }
+    }
+
+    createServer(): void {
+        // TODO Generate Game pointer
+        // this.join( .. );
+    }
+
+    join(gamePin): void {
+        this.gamePin = gamePin;
     }
 }
