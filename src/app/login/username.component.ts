874ec464b0776fef4e6aed6adf38ef3e1b58bf58
Rory Claasen <t-roclaa@microsoft.com>
2017-11-23 11:28:49 +0000

    little changes to logic

--- src/app/login/username.component.ts
+++ src/app/login/username.component.ts
@@ -6,32 +6,35 @@ import { Component } from '@angular/core';
 })
 
 export class UsernameComponent {
-    header: string = 'Tic-Tac-Toe';
-
     inputPrompt: string = 'Enter your username';
     inputLabel: string = 'Username';
     input: string = '';
-    inputLength: number = 24;
     inputType: string = 'text';
 
     username: string = '';
     gamePin: string = '';
-
+    
     validate(): void {
         var input = this.input;
         var length = input.length;
         if (length > 0) {
             if (this.username.length == 0) {
-                this.username = this.header = input;
+                this.username = input;
                 this.input = '';
                 this.inputPrompt = 'Enter the Game Pin or Create a server';
                 this.inputLabel = 'Game Pin';
-                this.inputLength = 6;
                 this.inputType = 'number';
             } else {
                 this.join(input);
             }
         }
+    }
+
+    resetInputs(): void {
+        this.username = this.input = '';
+        this.inputType = 'text';
+        this.inputPrompt = 'Enter your username';
+        this.inputLabel = 'Username';
     }
 
     createServer(): void {
