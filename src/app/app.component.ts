454b6423a2e53298b962f99efab7f3873fd6bdb7
Rory Claasen <t-roclaa@microsoft.com>
2017-12-01 16:26:45 +0000

    Working on grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,51 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
     title = 'TicTacToe';
+    navbar = false;
+
+    loaded = true; // false;
+
+    inputPrompt = 'Enter your username';
+    inputLabel = 'Username';
+    input = '';
+    inputType = 'text';
+
+    username = '';
+    gamePin = '';
+
+    validate(): void {
+        const input = this.input;
+        const length = input.length;
+        if (length > 0) {
+            if (this.username.length === 0) {
+                this.username = input;
+                this.input = '';
+                this.inputPrompt = 'Enter the Game Pin or Create a server';
+                this.inputLabel = 'Game Pin';
+                this.inputType = 'number';
+            } else {
+                this.join(input);
+            }
+        }
+    }
+
+    resetInputs(): void {
+        this.username = this.input = '';
+        this.inputType = 'text';
+        this.inputPrompt = 'Enter your username';
+        this.inputLabel = 'Username';
+    }
+
+    createServer(): void {
+        // TODO Generate Game pointer
+        // this.join( .. );
+    }
+
+    join(gamePin): void {
+        this.gamePin = gamePin;
+        this.loaded = true;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { UsernameComponent } from './login/username.component';
+import { GameComponent } from './game/game.compontent';
 
 @NgModule({
     declarations: [
         AppComponent,
-        UsernameComponent
+        GameComponent
     ],
     imports: [
         BrowserModule,
