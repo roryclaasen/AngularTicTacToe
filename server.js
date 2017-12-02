5ad2c539d99d04427217a99dd02379a83ddedd20
Rory Claasen <rory@claasen.biz>
2017-12-02 19:55:28 +0000

    'deploying...'

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/pusher/auth', function (req, res) {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
 var port = process.env.PORT || 3000;
