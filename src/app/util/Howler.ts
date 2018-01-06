43d1162586c98cfcbf031a0d3748ae51c4a02814
Rory Claasen <2751799+roryclaasen@users.noreply.github.com>
2018-01-06 18:41:14 +0000

    Update Howler.ts

--- src/app/util/Howler.ts
+++ src/app/util/Howler.ts
@@ -5,7 +5,7 @@ export class Howler {
     private static click: howlerjs.Howl[];
     public static load(): void {
         this.click = new Array<howlerjs.Howl>(9);
-        for (let i = 0; i < 9; i++) {
+        for (let i = 1; i <= 9; i++) {
             this.click[i] = new howlerjs.Howl({
                 src: ['./assets/note' + i + '.mp3']
             });
